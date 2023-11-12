7d26c1be6b738734f0b976ea1d975af712715d30
silvervelvet <108361662+silvervelvet@users.noreply.github.com>
2023-11-12 21:09:01 +0300

    refactoring

--- todolist-react-typescript/src/App.tsx
+++ todolist-react-typescript/src/App.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
-import TodoList from './Todolist';
+import TodoList, { TaskType} from './Todolist';
 import { v1 } from 'uuid';
 import AddItemForm from './AddItemForm';
 
 export type FilterValuesType = 'all' | 'completed' | 'active'
+
 type TodolistType = {
   id: string,
   title: string,
   filter: FilterValuesType
 }
 
-
+type TasksStateType = {
+  [key: string]: Array<TaskType>
+}
 
 function App() {
 
@@ -63,7 +66,7 @@ function App() {
     setTasks({...tasksObj})
   }
 
-  let [tasksObj, setTasks] = useState({
+  let [tasksObj, setTasks] = useState<TasksStateType>({
     [todolistId1]: [
       {id: v1(), title: 'CSS', isDone: true},
       {id: v1(), title: 'JS', isDone: true},
@@ -76,9 +79,19 @@ function App() {
     ]
   })
 
+  function addTodolist (title: string) {
+    let todolist: TodolistType = {
+      id: v1(),
+      filter: 'all',
+      title: title
+    }
+    setTodolists([todolist, ...todolists])
+    setTasks({...tasksObj, [todolist.id]: []})
+  }
+
   return (
     <div className="App">
-      <AddItemForm addItem={(title: string) => {}}/>
+      <AddItemForm addItem={addTodolist}/>
       {
         todolists.map((tl) => {
           let tasksForTodoList = tasksObj[tl.id];
