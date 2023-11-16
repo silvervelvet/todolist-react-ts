efe2ea95c537a7a87f0adf5db580057f8bf917a9
silvervelvet <108361662+silvervelvet@users.noreply.github.com>
2023-11-16 17:25:43 +0300

    refactoring reducer and unit test for todolists

--- todolist-react-typescript/src/state/todolist-reducer.test.ts
+++ todolist-react-typescript/src/state/todolist-reducer.test.ts
@@ -1,5 +1,5 @@
-import { TodolistType } from "../App";
-import {  RemoveTodolistAC, todolistsReducer, AddTodolistAC, ChangeTodolistFilterAC } from "./todolist-reducer";
+import { FilterValuesType, TodolistType } from "../App";
+import {  removeTodolistAC, todolistsReducer, addTodolistAC, changeTodolistFilterAC } from "./todolist-reducer";
 import { v1 } from "uuid";
 
 test('correct todolist should be removed', () => {
@@ -11,7 +11,7 @@ test('correct todolist should be removed', () => {
         {id: todolistId2, title: 'What to buy', filter: 'all'}
     ]
 
-    const endState = todolistsReducer(startState, RemoveTodolistAC(todolistId1))
+    const endState = todolistsReducer(startState, removeTodolistAC(todolistId1))
 
     expect(endState.length).toBe(1);
     expect(endState[0].id).toBe(todolistId2)
@@ -29,7 +29,7 @@ test('correct todolist should be added', () => {
         {id: todolistId2, title: 'What to buy', filter: 'all'}
     ]
 
-    const endState = todolistsReducer(startState, AddTodolistAC (newTodolistTitle))
+    const endState = todolistsReducer(startState, addTodolistAC (newTodolistTitle))
 
     expect(endState.length).toBe(3);
     expect(endState[2].title).toBe(newTodolistTitle);
@@ -63,14 +63,14 @@ test('correct filter of todolist should be changed', () => {
     let todolistId1 = v1();
     let todolistId2 = v1();
 
-    let newFilter = 'completed';
+    let newFilter: FilterValuesType = 'completed';
 
     const startState: Array<TodolistType> = [
         {id: todolistId1, title: 'What to learn', filter: 'all'},
         {id: todolistId2, title: 'What to buy', filter: 'all'}
     ]
 
-    const action =  ChangeTodolistFilterAC( todolistId2, newFilter ) 
+    const action =  changeTodolistFilterAC( todolistId2, newFilter ) 
 
     const endState = todolistsReducer (startState, action);
 
